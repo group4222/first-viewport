38c349df94faa13a84fa6de08e5da3c2a575ffb3
Kelvin Baiden <denlinato@gmail.com>
2021-05-16 02:08:13 +0000

    first view port

---
+++ src/component/Head.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import NavBAr from "./NavBar"
+import FirstMain from "./FirstMain"
+import Table from "./Table"
+class Head extends Component {
+    
+    render() { 
+        return ( 
+
+         <div>
+             <NavBAr/>
+             <FirstMain/>
+             </div>
+
+
+         );
+    }
+}
+ 
+export default Head;
